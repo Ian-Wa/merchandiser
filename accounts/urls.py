c0506a809616d42b2b2f1a7ea25f2c849358f743
moringa <ian.wanjira@student.moringaschool.com>
2022-01-27 08:14:18 +0300

    updated settings.py file

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
+
 from .views import  RegisterView, RetrieveUserView
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('register', RegisterView.as_view()),
     path('me', RetrieveUserView.as_view())
 ]
